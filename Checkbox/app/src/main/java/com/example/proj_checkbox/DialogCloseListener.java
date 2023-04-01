7ac8a1db33ef4560f8818e750fc4a9cd72ea5e78
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2023-04-01 11:49:00 -0400

    Delete Checkbox/app/src/main directory

--- Checkbox/app/src/main/java/com/example/proj_checkbox/DialogCloseListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.proj_checkbox;
-
-import android.content.DialogInterface;
-
-public interface DialogCloseListener
-{
-    public void handleDialogClose(DialogInterface dialogInterface);
-}
