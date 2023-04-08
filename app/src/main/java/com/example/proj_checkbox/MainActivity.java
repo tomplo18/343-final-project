660afbc7a64e67dd4d806e695367269f75b236a0
tomplo18 <t307468@gmail.com>
2023-04-08 15:47:04 -0400

    This is the main menu and Calendar additions

--- app/src/main/java/com/example/proj_checkbox/MainActivity.java
+++ app/src/main/java/com/example/proj_checkbox/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements DialogCloseListen
 
     private List<ToDoModel> taskList;
     private DatabaseHandler db;
+
+    Button toMainMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +64,14 @@ public class MainActivity extends AppCompatActivity implements DialogCloseListen
                 AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
 
             }
+        });
+
+        toMainMenu = (Button) findViewById(R.id.main_menu);
+
+        // button to main menu
+        toMainMenu.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MainMenu.class);
+            startActivity(intent);
         });
     }
 
