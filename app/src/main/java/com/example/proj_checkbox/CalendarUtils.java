d71fa96a33ff877f3ecb1c3147c58844f7e6517b
NehalKabir <92537145+NehalKabir@users.noreply.github.com>
2023-04-15 17:39:01 -0400

    Create CalendarUtils.java

---
+++ app/src/main/java/com/example/proj_checkbox/CalendarUtils.java
@@ -0,0 +1,51 @@
+package com.example.proj_checkbox;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+
+
+public class CalendarUtils {
+    public static LocalDate selectedDate;
+
+    public static String formattedDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    public static String formattedTime(LocalTime time)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        return time.format(formatter);
+    }
+
+    public static String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    public static ArrayList<LocalDate> daysInMonthArray(LocalDate date)
+    {
+        ArrayList<LocalDate> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = CalendarUtils.selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for(int i = 1; i <= 42; i++)
+        {
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
+                daysInMonthArray.add(null);
+            else
+                daysInMonthArray.add(LocalDate.of(selectedDate.getYear(),selectedDate.getMonth(),i - dayOfWeek));
+        }
+        return  daysInMonthArray;
+    }
+}
