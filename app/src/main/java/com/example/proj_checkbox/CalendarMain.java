4aaa16ccd0793062248b7cbd1abd147af8c21969
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2023-04-20 14:28:25 -0400

    Update CalendarMain.java

--- app/src/main/java/com/example/proj_checkbox/CalendarMain.java
+++ app/src/main/java/com/example/proj_checkbox/CalendarMain.java
@@ -16,7 +16,7 @@ import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
+public class CalendarMain extends AppCompatActivity implements CalendarAdapter.OnItemListener
 {
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
