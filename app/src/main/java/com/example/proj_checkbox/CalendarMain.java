15dedcc9fe1ce688548df641bbe83451f03fc4fc
NehalKabir <92537145+NehalKabir@users.noreply.github.com>
2023-04-20 14:36:55 -0400

    Update CalendarMain.java

--- app/src/main/java/com/example/proj_checkbox/CalendarMain.java
+++ app/src/main/java/com/example/proj_checkbox/CalendarMain.java
@@ -26,7 +26,7 @@ public class CalendarMain extends AppCompatActivity implements CalendarAdapter.O
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calendar_main);
         initWidgets();
         CalendarUtils.selectedDate = LocalDate.now();
         setMonthView();
